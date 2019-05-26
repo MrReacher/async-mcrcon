6ada54f963b948d573c3103e7ffab31984cc41b3
Reacher <support@mrreacher.com>
2019-05-27 00:12:22 +0300

    better naming & a useful check

--- async_mcrcon.py
+++ async_mcrcon.py
@@ -15,18 +15,19 @@ class MinecraftClient:
         self.password = password
 
         self._auth = None
-        self._read = None
-        self._write = None
+        self._reader = None
+        self._writer = None
 
     async def __aenter__(self):
-        self._read, self._write = await asyncio.open_connection(self.host, self.port)
-        await self._authenticate()
+        if not self._writer:
+            self._reader, self._writer = await asyncio.open_connection(self.host, self.port)
+            await self._authenticate()
 
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
-        if self._write:
-            self._write.close()
+        if self._writer:
+            self._writer.close()
 
     async def _authenticate(self):
         if not self._auth:
@@ -36,36 +37,33 @@ class MinecraftClient:
     async def _read_data(self, leng):
         data = b''
         while len(data) < leng:
-            data += await self._read.read(leng - len(data))
+            data += await self._reader.read(leng - len(data))
 
         return data
 
     async def _send(self, typen, message):
-        if not self._write:
-            return
+        if not self._writer:
+            raise ClientError('Not connected.')
 
         out = struct.pack('<li', 0, typen) + message.encode('utf8') + b'\x00\x00'
         out_len = struct.pack('<i', len(out))
-        self._write.write(out_len + out)
+        self._writer.write(out_len + out)
 
-        data = ''
-        while True:
-            in_len = struct.unpack('<i', await self._read_data(4))
-            in_payload = await self._read_data(in_len[0])
+        in_len = struct.unpack('<i', await self._read_data(4))
+        in_payload = await self._read_data(in_len[0])
 
-            in_id, in_type = struct.unpack('<ii', in_payload[:8])
-            in_data, in_padd = in_payload[8:-2], in_payload[-2:]
+        in_id, in_type = struct.unpack('<ii', in_payload[:8])
+        in_data, in_padd = in_payload[8:-2], in_payload[-2:]
 
-            if in_padd != b'\x00\x00':
-                raise ClientError('Incorrect padding.')
-            if in_id == -1:
-                raise InvalidPassword('Incorrect password.')
+        if in_padd != b'\x00\x00':
+            raise ClientError('Incorrect padding.')
+        if in_id == -1:
+            raise InvalidPassword('Incorrect password.')
 
-            data += in_data.decode('utf8')
-            
-            return data
+        data = in_data.decode('utf8')
+        return data
 
     async def send(self, cmd):
         result = await self._send(2, cmd)
-        await asyncio.sleep(0.003)
+        await asyncio.sleep(0.003) #unsure about this
         return result
